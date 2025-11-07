7de163bd8f526f3a693a4945a5f32aed314be4f5
AlfredHolley <alfredholley1@gmail.com>
2025-11-07 21:11:19 +0100

    change

--- app.py
+++ app.py
@@ -246,13 +246,14 @@ app.index_string = '''
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MJFVRND6');</script>
-        <!-- End Google Tag Manager -->
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CQJ1VFJ3S6"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CQJ1VFJ3S6');
+        </script>
         {%metas%}
         <title>Buchinger Science - Data</title>
         <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
@@ -267,10 +268,6 @@ app.index_string = '''
         {%css%}
     </head>
     <body>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJFVRND6"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         {%app_entry%}
         <footer>
             {%config%}
